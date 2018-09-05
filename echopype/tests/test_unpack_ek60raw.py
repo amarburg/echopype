5f9b5ae2fabb0f137b6a5a7c8a23a03a4a371f76
Aaron Marburg <amarburg@apl.washington.edu>
2018-09-05 13:58:52 -0700

    Added first test using Dyson EK60 data.

--- echopype/tests/test_unpack_ek60raw.py
+++ echopype/tests/test_unpack_ek60raw.py
@@ -1,8 +1,14 @@
 from echopype import unpack_ek60
 
+from os import path
+
+def data_file(filename):
+    return path.abspath(path.join(path.dirname(__file__),'..','data',filename))
+
 # OOI CE04OSPS EK60
-first_ping_metadata, data_times, power_data_dict, angle_data_dict, motion_data_dict, config_header, config_transducer = \
-    unpack_ek60.load_ek60_raw('data_zplsc/OOI-D20170821-T000000.raw')
+# def test_ooi_ek60_unpack():
+#     first_ping_metadata, data_times, power_data_dict, angle_data_dict, motion_data_dict, config_header, config_transducer = \
+#         unpack_ek60.load_ek60_raw(data_file('OOI-D20170821-T000000.raw'))
 
 # OceanStarr 2 channel EK60
 # first_ping_metadata, data_times, power_data_dict, frequencies, bin_size, config_header, config_transducer = \
@@ -16,3 +22,7 @@ first_ping_metadata, data_times, power_data_dict, angle_data_dict, motion_data_d
 # first_ping_metadata, data_times, power_data_dict, frequencies, bin_size, config_header, config_transducer = \
 #     unpack_ek60.load_ek60_raw('data_zplsc/D20180206-T000625.raw')
 
+
+def test_dyson_5channel_ek60_unpack():
+    first_ping_metadata, data_times, power_data_dict, angle_data_dict, motion_data_dict, config_header, config_transducer = \
+        unpack_ek60.load_ek60_raw(data_file('DY1801_EK60-D20180211-T164025.raw'))
